954e1eb43cd0bbeee50fcb30cb168206bf5ce738
pranay sawant <sawant.pranay.97@gmail.com>
2022-02-14 11:19:34 +0530

    adding other files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', loadChildren: () => import('./route-modules/profile/profile.module').then(m => m.ProfileModule) }, 
+  { path: 'dashboard', loadChildren: () => import('./route-modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'expenses', loadChildren: () => import('./route-modules/expenses/expenses.module').then(m => m.ExpensesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
